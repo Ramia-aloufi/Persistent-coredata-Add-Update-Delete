3bada8ac5fcd7d83b3b56d1575a607d02545c14d
RAM111111 <r.r.99@windowslive.com>
2021-12-14 16:27:19 +0300

    Initial Commit

---
+++ Update/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Update
+//
+//  Created by R on 10/05/1443 AH.
+//  Copyright © 1443 R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
